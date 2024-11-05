cbebb02d555bfc99ab7930e8419e6323fe3a2bb2
root <root@Zenbook-Samy.localdomain>
2024-11-06 00:31:26 +0100

    back and front behind src folder

---
+++ backend/MasterTagSystem/Models/Tag.cs
@@ -0,0 +1,9 @@
+namespace MasterTagSystem.Models
+{
+    public class Tag
+    {
+        public string Id { get; set; }
+        public string DestinationUrl { get; set; }
+        public string TrackingData { get; set; }
+    }
+}
--- src/backend/MasterTagSystem/Models/Tag.cs
+++
@@ -1,9 +0,0 @@
-namespace MasterTagSystem.Models
-{
-    public class Tag
-    {
-        public string Id { get; set; }
-        public string DestinationUrl { get; set; }
-        public string TrackingData { get; set; }
-    }
-}
