32d70453a9590f97e48be9d2c16e73bf89694398
root <root@Zenbook-Samy.localdomain>
2024-11-06 08:15:30 +0100

    reset version

--- frontend/frontend-app/src/app/app.module.ts
+++ frontend/frontend-app/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { IntegralUIModule } from '@lidorsystems/integralui-web/bin/integralui/integralui.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TagEditorComponent } from './components/tag-editor/tag-editor.component';
 import { TagValidatorComponent } from './components/tag-validator/tag-validator.component';
-import { TreeViewComponent } from './tree-view/tree-view.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +14,11 @@ import { TreeViewComponent } from './tree-view/tree-view.component';
     HeaderComponent,
     TagEditorComponent,
     TagValidatorComponent,
-    TreeViewComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    IntegralUIModule, // Ajout de IntegralUIModule ici
   ],
   providers: [],
   bootstrap: [AppComponent],
